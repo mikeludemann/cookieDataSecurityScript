f0897edfccc8b918ce4666291672a324c679d490
Mike Ludemann <mike-ludemann@hotmail.de>
2017-11-09 16:33:05 +0100

    First Commit with all data informations

---
+++ README.md
@@ -0,0 +1,18 @@
+# Cookie Data Security Script
+
+Layer for informations of using cookies
+
+## Setup
+
+Take the "cookie-script.js" or the minified version "cookie-script.min.js" and bind it in your Source Core
+
+```code
+<script src="/path/to/script/cookie-script.js"></script>
+<script src="/path/to/script/cookie-script.min.js"></script>
+```
+
+In "index.js" you can set all data and informations for this layer.
+
+```code
+<script src="/path/to/script/index.js"></script>
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+window.cookieconsent_options = {
+    message: 'This website uses cookies to provide the best possible functionality.',
+    dismiss: 'OK. Accepted',
+    learnMore: 'More Infos',
+    link: 'path/to/data-security-script',
+    theme: 'dark-top'
+};
+
+// 'message' is the text in this layer
+
+// 'dismiss' is the text on the Button
+
+// 'learnMore' is the text for this 'link' location
+
+// 'link' to the data policies
+
+// Possibilities of theme:
+// 'dark-top'
+// 'dark-bottom'
+// 'light-floating'
